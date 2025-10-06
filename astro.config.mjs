f05fb6f79f932aed2ac1c54b92413ae724628286
Matias <m.santossoza1304@gmail.com>
2025-10-06 17:03:34 -0300

    deploy: setup GitHub Pages manually

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://1xmxtixs.github.io/Solemne-1-Desarrollo-Web-/',
+  base: '/Solemne-1-Desarrollo-Web-/',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
