3f1a7dc227937d83551d318206c248b32857420a
Matias <m.santossoza1304@gmail.com>
2025-10-06 17:08:22 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://1xmxtixs.github.io/Solemne-1-Desarrollo-Web-/',
-  base: '/Solemne-1-Desarrollo-Web-/',
   vite: {
     plugins: [tailwindcss()],
   },
